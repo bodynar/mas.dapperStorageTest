0d7c14151257e6feb111e2df90d8230119ceac3b
bodynar <bodynar@gmail.com>
2020-12-06 16:52:04 +0300

    add database initializer

---
+++ MAS.DapperStorage.Test/Configuration/DbInitializer.cs
@@ -0,0 +1,36 @@
+﻿namespace MAS.DapperStorageTest.Configuration
+{
+    using System;
+
+    using MAS.DapperStorageTest.Infrastructure;
+
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
+    public static class DbInitializer
+    {
+        public static IHost InitializeDbIfNotExists(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    var dbConnectionFactory = scope.ServiceProvider.GetRequiredService<IDbConnectionFactory>();
+
+                    using (var connection = dbConnectionFactory.CreateDbConnection())
+                    {
+                        // todo: init db from sql file
+                        // add if else in sql
+                        var b = 10;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    var a = 10;
+                }
+            }
+
+            return host;
+        }
+    }
+}
