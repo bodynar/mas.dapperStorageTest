2d03109964e72ee9e4e8009906dd1a67868e76a2
bodynar <bodynar@gmail.com>
2020-12-06 03:34:27 +0300

    add insert command

---
+++ MAS.DappertStorageTest.Cqrs/Insert/InsertCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace MAS.DappertStorageTest.Cqrs
+{
+    using System;
+    using System.Collections.Generic;
+
+    using MAS.DapperStorageTest.Infrastructure.Cqrs;
+
+    public class InsertCommand : ICommand
+    {
+        public string EntityName { get; }
+
+        public IDictionary<string, object> PropertyValues { get; }
+
+        public InsertCommand(string entityName, IDictionary<string, object> propertyValues)
+        {
+            EntityName = entityName ?? throw new ArgumentNullException(nameof(entityName));
+            PropertyValues = propertyValues ?? throw new ArgumentNullException(nameof(propertyValues));
+        }
+    }
+}
---
+++ MAS.DappertStorageTest.Cqrs/Insert/InsertCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿namespace MAS.DappertStorageTest.Cqrs
+{
+    using System;
+    using System.Linq;
+
+    using Dapper;
+
+    using MAS.DapperStorageTest.Infrastructure;
+    using MAS.DappertStorageTest.Cqrs.Infrastructure;
+
+    public class InsertCommandHandler : BaseCommandHandler<InsertCommand>
+    {
+        public InsertCommandHandler(IDbConnectionFactory dbConnectionFactory)
+            : base(dbConnectionFactory)
+        {
+        }
+
+        public override void Handle(InsertCommand command)
+        {
+            EnsureEntityNameIsValid(command.EntityName);
+
+            var tableName = GetTableName(command.EntityName);
+            var fieldNames = string.Join(", ", command.PropertyValues.Keys.Select(key => $"[{key}]"));
+            var fieldValues = string.Join(", ", command.PropertyValues.Select(value => value));
+            var id = Guid.NewGuid();
+
+            using (var connection = DbConnectionFactory.CreateDbConnection())
+            {
+                var sqlQuery = $"INSERT INTO [{tableName}] ([Id], {fieldNames}) VALUES (@NewEntityId, {fieldValues})";
+                connection.Execute(sqlQuery, new { NewEntityId = id });
+            }
+        }
+    }
+}
