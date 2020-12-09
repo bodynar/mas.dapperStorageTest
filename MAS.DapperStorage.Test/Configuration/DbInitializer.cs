73f1e8319f94e4a8f791ad202d3d713d2db93ec9
bodynar <bodynar@gmail.com>
2020-12-10 02:04:07 +0300

    init db when app starts

--- MAS.DapperStorage.Test/Configuration/DbInitializer.cs
+++ MAS.DapperStorage.Test/Configuration/DbInitializer.cs
@@ -1,6 +1,8 @@
 ﻿namespace MAS.DapperStorageTest.Configuration
 {
     using System;
+    using System.IO;
+    using System.Reflection;
 
     using MAS.DapperStorageTest.Infrastructure;
 
@@ -9,6 +11,8 @@
 
     public static class DbInitializer
     {
+        private const string DatabaseInitFilename = "DbInit.sql";
+
         public static IHost InitializeDbIfNotExists(this IHost host)
         {
             using (var scope = host.Services.CreateScope())
@@ -17,11 +21,29 @@
                 {
                     var dbConnectionFactory = scope.ServiceProvider.GetRequiredService<IDbConnectionFactory>();
 
-                    using (var connection = dbConnectionFactory.CreateDbConnection())
+                    if (dbConnectionFactory != null)
                     {
-                        // todo: init db from sql file
-                        // add if else in sql
-                        var b = 10;
+                        var sqlDatabaseInitScript = GetSqlDataBaseInitScript();
+
+                        if (!string.IsNullOrEmpty(sqlDatabaseInitScript))
+                        {
+                            // TODO: When storageTest doesn't exist - here exception [StorageTest] not exist
+                            using (var connection = dbConnectionFactory.CreateDbConnection())
+                            {
+                                var command = connection.CreateCommand();
+
+                                command.CommandText = sqlDatabaseInitScript;
+
+                                connection.Open();
+                                if (connection.State == System.Data.ConnectionState.Open)
+                                {
+                                    command.ExecuteNonQuery();
+                                    connection.Close();
+                                    
+                                    // TODO: When storageTest not included in connection string and doesn't exist - here sql exception [StorageTest] not exists
+                                }
+                            }
+                        }
                     }
                 }
                 catch (Exception ex)
@@ -31,6 +53,24 @@
             }
 
             return host;
+        }
+
+        private static string GetSqlDataBaseInitScript()
+        {
+            var result = string.Empty;
+
+            try
+            {
+                var path = Path.Combine(
+                    Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                    DatabaseInitFilename
+                );
+
+                result = File.ReadAllText(path);
+            }
+            catch {}
+
+            return result;
         }
     }
 }
