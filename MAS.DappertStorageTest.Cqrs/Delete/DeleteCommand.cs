cea771d16500cfe704d09587da6dc49dbb862635
bodynar <bodynar@gmail.com>
2020-12-15 11:55:50 +0300

    add delete by filter option

--- MAS.DappertStorageTest.Cqrs/Delete/DeleteCommand.cs
+++ MAS.DappertStorageTest.Cqrs/Delete/DeleteCommand.cs
@@ -1,19 +1,30 @@
 ﻿namespace MAS.DappertStorageTest.Cqrs
 {
     using System;
+    using System.Collections.Generic;
 
     using MAS.DapperStorageTest.Infrastructure.Cqrs;
 
     public class DeleteCommand: ICommand
     {
-        public Guid EntityId { get; }
+        public Guid? EntityId { get; }
 
         public string EntityName { get; }
 
-        public DeleteCommand(Guid entityId, string entityName)
+        public IEnumerable<QueryFilter> Filters { get; }
+
+        public int RowsAffected { get; set; }
+
+        public DeleteCommand(string entityName, Guid entityId)
         {
-            EntityId = entityId;
             EntityName = entityName ?? throw new ArgumentNullException(nameof(entityName));
+            EntityId = entityId;
+        }
+
+        public DeleteCommand(string entityName, IEnumerable<QueryFilter> filters)
+        {
+            EntityName = entityName ?? throw new ArgumentNullException(nameof(entityName));
+            Filters = filters ?? throw new ArgumentNullException(nameof(filters));
         }
     }
 }
--- MAS.DappertStorageTest.Cqrs/Delete/DeleteCommandHandler.cs
+++ MAS.DappertStorageTest.Cqrs/Delete/DeleteCommandHandler.cs
@@ -16,11 +16,39 @@
         {
             EnsureEntityNameIsValid(command.EntityName);
 
+            var rowsAffected =
+                command.EntityId.HasValue
+                    ? DeleteById(command)
+                    : DeleteByFilters(command);
+
+            command.RowsAffected = rowsAffected;
+        }
+
+        private int DeleteById(DeleteCommand command)
+        {
+            var sqlQuery = BuildQuery($"DELETE FROM {command.EntityName} WHERE Id = @Id");
+            var result = 0;
+
             using (var connection = DbConnectionFactory.CreateDbConnection())
             {
-                var sqlQuery = $"DELETE FROM {command.EntityName} WHERE Id = @Id";
                 connection.Execute(sqlQuery, new { Id = command.EntityId });
             }
+
+            return result;
+        }
+
+        private int DeleteByFilters(DeleteCommand command)
+        {
+            var (whereCondition, arguments) = BuildWhereFilter(command.EntityName, command.Filters);
+            var sqlQuery = BuildQuery($"DELETE FROM {command.EntityName} WHERE {whereCondition}");
+            var result = 0;
+
+            using (var connection = DbConnectionFactory.CreateDbConnection())
+            {
+                connection.Execute(sqlQuery, arguments);
+            }
+
+            return result;
         }
     }
 }
