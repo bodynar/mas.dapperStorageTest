d99a2cded78ab1d9d7c3d2e9c18fc26148e217dd
bodynar <bodynar@gmail.com>
2020-12-18 00:24:21 +0300

    add entityName to queryFilter

--- MAS.DappertStorageTest.Cqrs/Infrastructure/QueryFilter/QueryFilter.cs
+++ MAS.DappertStorageTest.Cqrs/Infrastructure/QueryFilter/QueryFilter.cs
@@ -4,14 +4,17 @@
 
     public class QueryFilter
     {
+        public string EntityName { get; }
+
         public string FieldName { get; }
 
         public string FilterValue { get; }
 
         public ComparisonType ComparisonType { get; }
 
-        public QueryFilter(string fieldName, string filterValue, ComparisonType comparisonType)
+        public QueryFilter(string entityName, string fieldName, string filterValue, ComparisonType comparisonType)
         {
+            EntityName = entityName ?? throw new ArgumentNullException(nameof(entityName));
             FieldName = fieldName ?? throw new ArgumentNullException(nameof(fieldName));
             FilterValue = filterValue ?? throw new ArgumentNullException(nameof(filterValue));
             ComparisonType = comparisonType;
