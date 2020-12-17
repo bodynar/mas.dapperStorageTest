e9135f78529874eda68b5eda573099301b025a65
bodynar <bodynar@gmail.com>
2020-12-18 00:24:33 +0300

    add filter join type enum and filter utils

---
+++ MAS.DappertStorageTest.Cqrs/Infrastructure/QueryFilter/Enums/FilterJoinType.cs
@@ -0,0 +1,11 @@
+﻿namespace MAS.DappertStorageTest.Cqrs
+{
+    public enum FilterJoinType
+    {
+        None = 0,
+
+        And = 1,
+
+        Or = 2
+    }
+}
---
+++ MAS.DappertStorageTest.Cqrs/Infrastructure/QueryFilter/FilterUtils.cs
@@ -0,0 +1,26 @@
+﻿namespace MAS.DappertStorageTest.Cqrs.Infrastructure
+{
+    public static class FilterUtils
+    {
+        public static string GetComparisonOperator(this ComparisonType comparisonType)
+        {
+            var field = comparisonType.GetType().GetField(comparisonType.ToString());
+
+            if (field == null)
+            {
+                return string.Empty;
+            }
+
+            var comparisonOperatorAttributes =
+                field.GetCustomAttributes(typeof(ComparisonOperatorAttribute), false) as ComparisonOperatorAttribute[];
+
+            if (comparisonOperatorAttributes != null
+                && comparisonOperatorAttributes.Length > 0)
+            {
+                return comparisonOperatorAttributes[0].Operator;
+            }
+
+            return string.Empty;
+        }
+    }
+}
