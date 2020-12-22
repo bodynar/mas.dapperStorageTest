86f5160d965b322fb2b6aa8c4c9b32cbc4584b3d
bodynar <bodynar@gmail.com>
2020-12-22 13:54:33 +0300

    add logger contract

---
+++ MAS.DapperStrorageTest.Infrastructure/Contracts/Logger/ILogger.cs
@@ -0,0 +1,24 @@
+﻿namespace MAS.DapperStorageTest.Infrastructure
+{
+    public enum LogLevel
+    {
+        Default = 0,
+        Debug = 1,
+        Info = 2,
+        Warn = 3,
+        Error = 4
+    }
+
+    public interface ILogger
+    {
+        void Log(LogLevel logLevel, string message);
+
+        void Debug(string message);
+
+        void Info(string message);
+
+        void Warn(string message);
+
+        void Error(string message);
+    }
+}
