9aca74df5d6e02ab05c40941c5c23c8bbecedbe0
bodynar <bodynar@gmail.com>
2020-12-22 13:55:15 +0300

    add logger baseline

---
+++ MAS.DapperStorage.Test/Configuration/Types/Logger.cs
@@ -0,0 +1,34 @@
+﻿namespace MAS.DapperStorageTest
+{
+    using System;
+
+    using MAS.DapperStorageTest.Infrastructure;
+
+    public class Logger : ILogger
+    {
+        public void Debug(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Info(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Log(LogLevel logLevel, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warn(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
