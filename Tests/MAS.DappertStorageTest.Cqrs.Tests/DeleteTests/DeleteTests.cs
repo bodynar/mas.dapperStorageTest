1777ebd503e13ff5915bb3481a93ed4699e26b45
bodynar <bodynar@gmail.com>
2021-01-18 01:44:08 +0300

    temporarily comment delete tests

--- Tests/MAS.DappertStorageTest.Cqrs.Tests/DeleteTests/DeleteTests.cs
+++ Tests/MAS.DappertStorageTest.Cqrs.Tests/DeleteTests/DeleteTests.cs
@@ -8,34 +8,34 @@
 
     public sealed class DeleteTests : BaseCqrsTests
     {
-        [Fact]
-        public void ShouldThrowDatabaseOperationExceptionWhenEntityNameIsNotValid()
-        {
-            var entityName = "TESTENTITYNAME";
-            var expectedExceptionMessage = "Entity name \"TESTENTITYNAME\" is not valid or isn't presented in database.";
-            var command = new DeleteCommand(entityName, Guid.Empty);
-            var handler = new DeleteCommandHandler(DbConnectionFactory, FilterBuilder);
+        //[Fact]
+        //public void ShouldThrowDatabaseOperationExceptionWhenEntityNameIsNotValid()
+        //{
+        //    var entityName = "TESTENTITYNAME";
+        //    var expectedExceptionMessage = "Entity name \"TESTENTITYNAME\" is not valid or isn't presented in database.";
+        //    var command = new DeleteCommand(entityName, Guid.Empty);
+        //    var handler = new DeleteCommandHandler(DbConnectionFactory, FilterBuilder);
 
-            var exception =
-                Record.Exception(
-                    () => handler.Handle(command)
-                );
+        //    var exception =
+        //        Record.Exception(
+        //            () => handler.Handle(command)
+        //        );
 
-            Assert.NotNull(exception);
-            Assert.IsType<DatabaseOperationException>(exception);
-            Assert.Equal(expectedExceptionMessage, exception.Message);
-        }
+        //    Assert.NotNull(exception);
+        //    Assert.IsType<DatabaseOperationException>(exception);
+        //    Assert.Equal(expectedExceptionMessage, exception.Message);
+        //}
 
-        [Fact]
-        public void Should()
-        {
-            // TODO: replace this config with InMemoryRepo (sql lite or ormlite)
+        //[Fact]
+        //public void Should()
+        //{
+        //    // TODO: replace this config with InMemoryRepo (sql lite or ormlite)
 
-            var entityName = "Driver";
-            var command = new DeleteCommand(entityName, Guid.Empty);
-            var handler = new DeleteCommandHandler(DbConnectionFactory, FilterBuilder);
+        //    var entityName = "Driver";
+        //    var command = new DeleteCommand(entityName, Guid.Empty);
+        //    var handler = new DeleteCommandHandler(DbConnectionFactory, FilterBuilder);
 
-            //handler.Handle(command);
-        }
+        //    //handler.Handle(command);
+        //}
     }
 }
