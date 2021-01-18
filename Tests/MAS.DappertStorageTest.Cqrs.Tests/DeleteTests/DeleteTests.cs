91a52316cc45bd72c8ebbf7e91e5d5eea5e73a21
bodynar <bodynar@gmail.com>
2021-01-18 15:15:12 +0300

    rework delete tests

--- Tests/MAS.DappertStorageTest.Cqrs.Tests/DeleteTests/DeleteTests.cs
+++ Tests/MAS.DappertStorageTest.Cqrs.Tests/DeleteTests/DeleteTests.cs
@@ -1,41 +1,49 @@
 ﻿namespace MAS.DappertStorageTest.Cqrs.Tests
 {
     using System;
+    using System.Collections.Generic;
 
     using MAS.DapperStorageTest.Infrastructure;
+    using MAS.DapperStorageTest.Models;
 
     using Xunit;
 
     public sealed class DeleteTests : BaseCqrsTests
     {
-        //[Fact]
-        //public void ShouldThrowDatabaseOperationExceptionWhenEntityNameIsNotValid()
-        //{
-        //    var entityName = "TESTENTITYNAME";
-        //    var expectedExceptionMessage = "Entity name \"TESTENTITYNAME\" is not valid or isn't presented in database.";
-        //    var command = new DeleteCommand(entityName, Guid.Empty);
-        //    var handler = new DeleteCommandHandler(DbConnectionFactory, FilterBuilder);
+        [Fact]
+        public void ShouldThrowDatabaseOperationExceptionWhenEntityNameIsNotValid()
+        {
+            var entityName = "TESTENTITYNAME";
+            var expectedExceptionMessage = "Entity name \"TESTENTITYNAME\" is not valid or isn't presented in database.";
+            var command = new DeleteCommand(entityName, Guid.Empty);
+            var handler = new DeleteCommandHandler(DbConnectionFactory, DbAdapter, FilterBuilder);
 
-        //    var exception =
-        //        Record.Exception(
-        //            () => handler.Handle(command)
-        //        );
+            var exception =
+                Record.Exception(
+                    () => handler.Handle(command)
+                );
 
-        //    Assert.NotNull(exception);
-        //    Assert.IsType<DatabaseOperationException>(exception);
-        //    Assert.Equal(expectedExceptionMessage, exception.Message);
-        //}
+            Assert.NotNull(exception);
+            Assert.IsType<DatabaseOperationException>(exception);
+            Assert.Equal(expectedExceptionMessage, exception.Message);
+        }
 
-        //[Fact]
-        //public void Should()
-        //{
-        //    // TODO: replace this config with InMemoryRepo (sql lite or ormlite)
+        [Fact]
+        public void ShouldBuildSqlQueryFromIdParameter()
+        {
+            var testedGuid = Guid.NewGuid();
+            var expectedSql = "DELETE FROM [Passenger] WHERE Id = @Id";
+            var expectedArguments = new[] { new KeyValuePair<string, object>("Id", testedGuid) };
+            var entityName = nameof(Passenger);
+            var command = new DeleteCommand(entityName, testedGuid);
+            var handler = new DeleteCommandHandler(DbConnectionFactory, DbAdapter, FilterBuilder);
 
-        //    var entityName = "Driver";
-        //    var command = new DeleteCommand(entityName, Guid.Empty);
-        //    var handler = new DeleteCommandHandler(DbConnectionFactory, FilterBuilder);
+            handler.Handle(command);
+            var lastCommand = GetLastCommand();
 
-        //    //handler.Handle(command);
-        //}
+            AssertSqlQuery(expectedSql, lastCommand.Key);
+            Assert.NotNull(lastCommand.Value);
+            AssertArguments(lastCommand.Value, expectedArguments);
+        }
     }
 }
